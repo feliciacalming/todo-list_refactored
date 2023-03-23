fcb35a6e0e9f7277ffd95b3698e2c9effd484032
feliciacalming <felicia.calming@gmail.com>
2023-03-23 21:42:54 +0100

    (refactor): update getFromLocalStorage

--- src/ts/helpers/localStorage.ts
+++ src/ts/helpers/localStorage.ts
@@ -1,14 +1,13 @@
-import { createToDoList } from '../createToDoList';
 import { ToDo } from '../models/Todo';
 
 export function saveToLocalStorage(toDoList: ToDo[]): void {
   localStorage.setItem('to-do', JSON.stringify(toDoList));
 }
 
-export function getFromLocalStorage(): void {
+export function getFromLocalStorage(): ToDo[] {
   const toDoList = (JSON.parse(localStorage.getItem('to-do') as string) as ToDo[]).map(todo => {
     return new ToDo(todo.task, todo.completed);
   });
 
-  createToDoList(toDoList);
+  return toDoList;
 }
