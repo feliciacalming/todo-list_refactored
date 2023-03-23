230b4388071fd15dc1f6034cc6c473a3f17a4626
feliciacalming <felicia.calming@gmail.com>
2023-03-23 15:42:50 +0100

    (refactor): move createToDoList from main.ts

---
+++ src/ts/createToDoList.ts
@@ -0,0 +1,47 @@
+import type { ToDo } from './models/Todo';
+
+const list: HTMLUListElement = document.createElement('ul');
+const listContainer: HTMLDivElement = document.getElementById('uncompleted-tasks') as HTMLDivElement;
+listContainer.classList.add('list-container');
+export function createToDoList(toDoList: ToDo[]): void {
+  list.innerHTML = '';
+  for (let i = 0; i < toDoList.length; i++) {
+    const itemContainer = document.createElement('span');
+    const task = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const deleteButton = document.createElement('button');
+
+    list.classList.add('list');
+    itemContainer.classList.add('list__item__container');
+    task.classList.add('list__item');
+    checkbox.classList.add('list__checkbox');
+    deleteButton.classList.add('list__deletebutton');
+
+    task.innerHTML = toDoList[i].task;
+
+    checkbox.type = 'checkbox';
+    checkbox.checked = toDoList[i].completed;
+    checkbox.addEventListener('click', () => {
+      if (checkbox.checked) {
+        toDoList[i].completed = true;
+        task.classList.add('list__item--completed');
+      } else {
+        toDoList[i].completed = false;
+        task.classList.remove('list__item--completed');
+      }
+      localStorage.setItem('to-do', JSON.stringify(toDoList));
+    });
+
+    deleteButton.innerHTML = "<i class='bi bi-trash'></i>";
+    deleteButton.addEventListener('click', () => {
+      toDoList.splice(i, 1);
+      localStorage.setItem('to-do', JSON.stringify(toDoList));
+      createToDoList(toDoList);
+    });
+
+    task.appendChild(checkbox);
+    task.appendChild(deleteButton);
+    list.appendChild(task);
+    listContainer.append(list);
+  }
+}
