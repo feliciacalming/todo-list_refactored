bbd1aa560c0afab902a5e8e5f66b184db62598cc
feliciacalming <felicia.calming@gmail.com>
2023-03-24 10:44:02 +0100

    (refactor): delete createHtml.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-import { createHtml } from './src/ts/createHtml';
 import { createToDoList } from './src/ts/createToDoList';
 import { getFromLocalStorage } from './src/ts/helpers/localStorage';
 import type { ToDo } from './src/ts/models/todo';
@@ -6,5 +5,4 @@ import type { ToDo } from './src/ts/models/todo';
 export const toDoList: ToDo[] = getFromLocalStorage();
 
 createToDoList();
-createHtml();
 // setCaret();
